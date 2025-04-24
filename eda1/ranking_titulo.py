01ae2965a2234931f281e442df77ee3d7068cc14
Lucas Costa <lucas.piccioni_costa@jku.at>
2025-04-24 16:11:58 +0200

    pagina de ranking de titulo no mais organizada

--- eda1/ranking_titulo.py
+++ eda1/ranking_titulo.py
@@ -1,4 +1,89 @@
 import streamlit as st
+import pandas as pd
+from pipeline import hinos_processados
 
-st.markdown("# Page 3 ❄️")
-st.sidebar.markdown("# Page 3 ❄️")
+hinos = hinos_processados()
+
+# separa dados de interesse
+hinos_analise = (
+    hinos[["numero", "nome"]]
+    .rename(columns={"numero": "Nº", "nome": "Nome"})
+    .set_index("Nº")
+)
+# separa subtitulo do nome
+hinos_analise["subtitulo"] = (
+    hinos_analise["Nome"].str.extract(r"\((.*?)\)").squeeze().str.strip()
+)
+hinos_analise["Nome"] = hinos_analise["Nome"].str.replace(
+    r"\s*\(.*?\)\s*", "", regex=True
+)
+# cria dataframe comparativo, considerando o subtitulo como um nome diferente
+hinos_titulos = pd.concat(
+    [
+        hinos_analise[["subtitulo"]].rename(columns={"subtitulo": "Nome"}),
+        hinos_analise[["Nome"]],
+    ]
+).dropna()
+# calcula o tamanho do titulo
+hinos_analise["titulo_tam_real"] = hinos_analise["Nome"].str.len()
+hinos_titulos["titulo_tam_real"] = hinos_titulos["Nome"].str.len()
+
+
+st.markdown("# Tamanho dos títulos")
+# st.sidebar.markdown("# Tamanho dos títulos")
+
+
+st.markdown("Top 10 maiores títulos")
+st.dataframe(
+    hinos_analise[["Nome", "titulo_tam_real"]]
+    .sort_values(by="titulo_tam_real", ascending=False)
+    .head(10),
+    column_config={
+        "titulo_tam_real": st.column_config.ProgressColumn(
+            "Tamanho",
+            format="%f",
+            help="Tamanho do título em caracteres",
+            max_value=int(hinos_analise["titulo_tam_real"].max()),
+        )
+    },
+)
+st.markdown("Top 10 menores títulos")
+st.dataframe(
+    hinos_analise[["Nome", "titulo_tam_real"]]
+    .sort_values(by="titulo_tam_real")
+    .head(10),
+    column_config={
+        "titulo_tam_real": st.column_config.ProgressColumn(
+            "Tamanho",
+            format="%f",
+            help="Tamanho do título em caracteres",
+            max_value=int(hinos_analise["titulo_tam_real"].max()),
+        )
+    },
+)
+
+
+st.markdown("Top 10 maiores títulos")
+st.dataframe(
+    hinos_titulos.sort_values(by="titulo_tam_real", ascending=False).head(10),
+    column_config={
+        "titulo_tam_real": st.column_config.ProgressColumn(
+            "Tamanho",
+            format="%f",
+            help="Tamanho do título em caracteres",
+            max_value=int(hinos_titulos["titulo_tam_real"].max()),
+        )
+    },
+)
+st.markdown("Top 10 menores títulos")
+st.dataframe(
+    hinos_titulos.sort_values(by="titulo_tam_real").head(10),
+    column_config={
+        "titulo_tam_real": st.column_config.ProgressColumn(
+            "Tamanho",
+            format="%f",
+            help="Tamanho do título em caracteres",
+            max_value=int(hinos_titulos["titulo_tam_real"].max()),
+        )
+    },
+)

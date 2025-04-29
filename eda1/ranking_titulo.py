c86e5175fd3ce923fc42876e78472a4135e19a08
Lucas Costa <lucas.piccioni_costa@jku.at>
2025-04-29 17:08:55 +0200

    layout ok

--- eda1/ranking_titulo.py
+++ eda1/ranking_titulo.py
@@ -33,57 +33,71 @@ st.markdown("# Tamanho dos títulos")
 # st.sidebar.markdown("# Tamanho dos títulos")
 
 
-st.markdown("Top 10 maiores títulos")
-st.dataframe(
-    hinos_analise[["Nome", "titulo_tam_real"]]
-    .sort_values(by="titulo_tam_real", ascending=False)
-    .head(10),
-    column_config={
-        "titulo_tam_real": st.column_config.ProgressColumn(
-            "Tamanho",
-            format="%f",
-            help="Tamanho do título em caracteres",
-            max_value=int(hinos_analise["titulo_tam_real"].max()),
-        )
-    },
-)
-st.markdown("Top 10 menores títulos")
-st.dataframe(
-    hinos_analise[["Nome", "titulo_tam_real"]]
-    .sort_values(by="titulo_tam_real")
-    .head(10),
-    column_config={
-        "titulo_tam_real": st.column_config.ProgressColumn(
-            "Tamanho",
-            format="%f",
-            help="Tamanho do título em caracteres",
-            max_value=int(hinos_analise["titulo_tam_real"].max()),
-        )
-    },
-)
+col1, col2 = st.columns(2)
 
 
-st.markdown("Top 10 maiores títulos")
-st.dataframe(
-    hinos_titulos.sort_values(by="titulo_tam_real", ascending=False).head(10),
-    column_config={
-        "titulo_tam_real": st.column_config.ProgressColumn(
-            "Tamanho",
-            format="%f",
-            help="Tamanho do título em caracteres",
-            max_value=int(hinos_titulos["titulo_tam_real"].max()),
-        )
-    },
-)
-st.markdown("Top 10 menores títulos")
-st.dataframe(
-    hinos_titulos.sort_values(by="titulo_tam_real").head(10),
-    column_config={
-        "titulo_tam_real": st.column_config.ProgressColumn(
-            "Tamanho",
-            format="%f",
-            help="Tamanho do título em caracteres",
-            max_value=int(hinos_titulos["titulo_tam_real"].max()),
-        )
-    },
-)
+with col1:
+    st.markdown("**Desconsiderando subtítulos**")
+    st.markdown("Top 10 maiores títulos")
+    st.dataframe(
+        hinos_analise[["Nome", "titulo_tam_real"]]
+        .sort_values(by="titulo_tam_real", ascending=False)
+        .head(10),
+        column_config={
+            "titulo_tam_real": st.column_config.ProgressColumn(
+                "Tamanho",
+                format="%f",
+                help="Tamanho do título em caracteres",
+                max_value=int(hinos_analise["titulo_tam_real"].max()),
+                width="small",
+            ),
+            "Nome": st.column_config.TextColumn(width="small", max_chars=25),
+        },
+    )
+    st.markdown("Top 10 menores títulos")
+    st.dataframe(
+        hinos_analise[["Nome", "titulo_tam_real"]]
+        .sort_values(by="titulo_tam_real")
+        .head(10),
+        column_config={
+            "titulo_tam_real": st.column_config.ProgressColumn(
+                "Tamanho",
+                format="%f",
+                help="Tamanho do título em caracteres",
+                max_value=int(hinos_analise["titulo_tam_real"].max()),
+                width="small",
+            ),
+            "Nome": st.column_config.TextColumn(width="small", max_chars=25),
+        },
+    )
+
+with col2:
+    st.markdown("**Considerando subtítulos**")
+    st.markdown("Top 10 maiores títulos")
+    st.dataframe(
+        hinos_titulos.sort_values(by="titulo_tam_real", ascending=False).head(10),
+        column_config={
+            "titulo_tam_real": st.column_config.ProgressColumn(
+                "Tamanho",
+                format="%f",
+                help="Tamanho do título em caracteres",
+                max_value=int(hinos_titulos["titulo_tam_real"].max()),
+                width="small",
+            ),
+            "Nome": st.column_config.TextColumn(width="small", max_chars=25),
+        },
+    )
+    st.markdown("Top 10 menores títulos")
+    st.dataframe(
+        hinos_titulos.sort_values(by="titulo_tam_real").head(10),
+        column_config={
+            "titulo_tam_real": st.column_config.ProgressColumn(
+                "Tamanho",
+                format="%f",
+                help="Tamanho do título em caracteres",
+                max_value=int(hinos_titulos["titulo_tam_real"].max()),
+                width="small",
+            ),
+            "Nome": st.column_config.TextColumn(width="small", max_chars=25),
+        },
+    )

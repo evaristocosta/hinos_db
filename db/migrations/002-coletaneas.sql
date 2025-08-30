b836dc346d53b80c005a2fbe44659f5ed8bf2c36
Lucas Costa <lucascosta74@gmail.com>
2025-08-30 13:30:51 +0200

    interseccao de hinos avulsos do drive do departamento com os da base pra definir faltantes

--- db/migrations/002-coletaneas.sql
+++ db/migrations/002-coletaneas.sql
@@ -2,3 +2,4 @@ INSERT INTO coletanea (nome, descricao, arquivo) VALUES ('COLETÂNEA DE LOUVORES
 INSERT INTO coletanea (nome, descricao, arquivo) VALUES ('COLETÂNEA DE CRIANÇAS - IGREJA CRISTÃ MARANATA - EDIÇÃO 2019', 'ATUALIZAÇÃO  11.07.2021. Origem: slides', '03.COLETÂNEA DE CIAS_2021 TV.pptx');
 INSERT INTO coletanea (nome, descricao, arquivo) VALUES ('Louvores Avulsos de Crianças, Intermediários e Adolescentes - Edição 2022', 'Origem: slides', 'Louvores Avulsos CIAs 2022 - COM ANIMAÇÃO.pptx');
 INSERT INTO coletanea (nome, descricao, arquivo) VALUES ('LOUVORES AVULSOS', 'REV 31.12.22. Origem: slides', 'LOUVORES AVULSOS_Rev_31.12.22.pptx');
+INSERT INTO coletanea (nome, descricao, arquivo) VALUES ('LOUVORES AVULSOS - MANUAL', '20250838. Origem: extração de listagem de arquivos do drive do Departamento de Louvor', '');
